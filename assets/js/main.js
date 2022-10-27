7af4f56b99c15cba465c728d527ed673ffae98ce
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-27 17:14:55 +0200

    ragionamento "pari e disapri"

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,7 @@ Creare una funzione per capire se la parola inserita è palindroma
 // chiedere all'utente di inserire  una parola
 //funzone palindrmoa
 
+/*
 let nomeUtente = prompt("inserisci una parola");
 
 
@@ -33,6 +34,10 @@ if (nomeUtente == nomeInverso){
     console.log('la parola non è palindroma')
 }
 
+/*
+
+
+
 
 
 /*Pari e Dispari
@@ -41,6 +46,62 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
+
+
+// l'utente inserisce da prompt se pari o dispari
+
+// se l'utente sceglie pari il computer sceglie dispari
+
+//se l'utente sceglie dispari il computer sceglie pari
+
+//creo una funzione che genera numeri  (1-5) v
+
+//creo una funzione che somma due numeri 
+
+//creo una funzione che determina se un numero è pari o dispari 
+
+
+//funzioni
+//numero Random
+
+function randomNumber() {
+    
+    return num = Math.floor(Math.random() * 5) + 1 ;
+     
+}
+
+//Somma due numeri
+
+function sommaNumeri(num1,num2){
+    return num1 + num2 
+}
+
+//pari o dispari
+
+function pariDispari(){
+    
+    let pari = 0;
+    
+    if (pari % 2 == 0 ){
+        console.log('il numero è pari')
+    }else{
+       console.log('il numero è dispari') 
+    }
+   
+}
+
+let k = parseInt(prompt("inserisci un numero"));
+
+k = pariDispari();
+console.log(k);
+
+
+
+
+
+
+
+
 
 
 
