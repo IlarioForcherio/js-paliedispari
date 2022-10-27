065b88709748057452ae327c7d6ef71f1c8460e0
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-27 15:41:48 +0200

    parola palindroma

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+
+
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+// chiedere all'utente di inserire  una parola
+//funzone palindrmoa
+
+let nomeUtente = prompt("inserisci una parola");
+
+
+
+function parolaDivisa(reverse){
+ let reverseWord = reverse.split('').reverse().join('');
+ return  reverseWord;
+
+}
+
+
+if (nomeUtente == nomeInverso){
+    console.log("la parola è palindroma" );
+}else{
+    console.log('la parola non è palindroma')
+}
+
+
+
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
+
+
+
+
+
+
+
+
